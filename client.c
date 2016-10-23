46ffd6a0a1fa91575633d41987ddc08b6bb59fa8
jessicaprt <jessicaprieto31@gmail.com>
2016-10-23 15:35:18 -0600

    get handshake working(ish)

--- client.c
+++ client.c
@@ -7,14 +7,15 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
-
 int main (int argc, char * argv[]) { //input	: chat379 hostname portnumber username
 	struct sockaddr_in srv_addr;
 	struct sockaddr_in cli_addr;
 	struct hostent *server;
+	char buffer[255];
+	ssize_t read_size;
 
 	if (argc < 4) {
-		fprintf("Error, too few arguments");
+		printf("Error, too few arguments");
 		exit(1);
 	}
 
@@ -22,10 +23,11 @@ int main (int argc, char * argv[]) { //input	: chat379 hostname portnumber usern
 	int portnumber = atoi(argv[2]);
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == -1) {
-		fprintf("Error opening");
+		printf("Error opening\n");
 		exit(1);
 	}
-	server = gethostname(argv[1]);
+
+	server = gethostbyname(argv[1]);
 	if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
@@ -34,29 +36,46 @@ int main (int argc, char * argv[]) { //input	: chat379 hostname portnumber usern
     bzero((char *) &srv_addr, sizeof(srv_addr));
     bcopy((char *)server->h_addr, (char *)&srv_addr.sin_addr.s_addr, server->h_length);
     srv_addr.sin_family = AF_INET;
-    srv_addr.sin_port = htons(portno);
+    srv_addr.sin_port = htons(portnumber);
 
     //establish connection to server
-    if(connect(sock, &srv_addr, sizeof(srv_addr)) == -1) {
-    	perror("Error connnecting");
+    if(connect(sock,(struct sockaddr *) &srv_addr, sizeof(srv_addr)) == -1) {
+    	perror("Error connnecting\n");
+    } else {
+    	printf("c--connect success\n");
     }
 
     //Acknowledgement process
-    int number = 0;
-	recv(sock, (char*)&number, sizeof(number), 0);
-	if(number[0] != 0xCF || number[1] || 0xA7){
-		perror("Acknowledgement Failed");
+    char ackbuffer[2];
+	unsigned char byte0, byte1;
+
+    // printf("entering recv\n");
+	while (read_size = recv(sock, (char*)&ackbuffer, 2, 0) > 0){
+		printf("recieveing\n");
+		printf ("%i\n", read_size);
+		byte0 = *((unsigned char *)&ackbuffer + 0);
+		printf("byte0 %x\n", byte0);
+		byte1 = *((unsigned char *)&ackbuffer + 1);
+		printf("byte1 %x\n", byte1);
 	}
+	
+	// recv(sock, (char*)&ackbuffer, 2, 0);
+	
+	if (byte0 != 0xcf || byte1 != 0xa7) printf("byte failed\n");
+	else printf("ack success");
 
 	/**** get username info ****/
 	int length = strlen(username);
 	int name[2];
 	name[0] = length; name[1] = atoi(username);
-	send(sock, (char*)&name, sizeof(name), 0);
+	write(sock, (char*)&name, sizeof(name) );
 
 	/** check if username exists 
 	 *  terminate connection if it exists
 	**/
 
+	//chat connection
+
+    close(sock);
 	return 0;
 }
--- server.c
+++ server.c
@@ -2,6 +2,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdlib.h>
+#include <strings.h>
 
 /***
 struct sockaddr_in
@@ -15,56 +17,91 @@ struct in_addr
 
 ****/
 
+typedef int bool;
+#define true 1
+#define false 0
+
 int main(int argc, char * argv[]) { //input		: server379 portnumber
 	struct sockaddr_in srv_addr;
 	struct sockaddr_in cli_addr;
 	int numconnections = 5; // HOW TO GET NUMBER OF USERS CONNECTED?
+	char buffer[255];
 	
-	if (argc < 2) {
-		fprintf("Error, too few arguments");
-		exit(1);
-	}
+	// if (argc < 1) {
+	// 	printf("Error, too few arguments");
+	// 	exit(1);
+	// }
 
 	/** send the following two bytes 0xCF 0xA7 **/
 	int portnum = atoi(argv[1]);
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (sock == -1) {
-		fprintf("Error opening");
+		printf("Error opening");
 		exit(1);
 	}
 
 	//set all values in a buffer to zero, initialize srv_addr to zero
-    bzero((char *) &serv_addr, sizeof(srv_addr));
+    bzero((char *) &srv_addr, sizeof(srv_addr));
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_addr.s_addr = INADDR_ANY;
-    srv_addr.sin_port = htons(portnumber); //in network byte order
+    srv_addr.sin_port = htons(portnum); //in network byte order
 
     //binding to socket
-    if(bind(sock, (struck sockaddr*) &srv_addr, sizeof(srv_addr)) == -1) {
+    if(bind(sock, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) == -1) {
     	perror("Failed to bind socket");
     }
 
     listen(sock, numconnections);
 
-    int acceptsock = accept(sock, (struct sockaddr*) &cli_addr, sizeof(cli_addr));
+    socklen_t cli_size = sizeof(cli_addr);
+
+    int acceptsock = accept(sock, (struct sockaddr*) &cli_addr, &cli_size);
     if(acceptsock == -1) {
     	perror("Error accepting");
     	exit(1);
+    } else {
+    	printf("s-- connect success\n");
     }
 
 	// Acknowledgement process
-	int ACK[2];
-	ACK[0] = 0xCF; ACK[1] = 0xA7;
-	send(sock, (char*)&ACK, sizeof(ACK), 0);
+	// int ACK[2];
+	// ACK[0] = 0xCF; ACK[1] = 0xA7;
+	unsigned char byte;
+	printf("sending ack\n");
+	char* ackbuffer[2];
+	sprintf(ackbuffer, "\xCF\xA7\n");
+	byte = *((unsigned char *)&ackbuffer + 0);
+	printf("server buffer %x \n", byte);
+	byte = *((unsigned char *)&ackbuffer + 1);
+	printf("server buffer %x \n", byte);
 
-	unsigned int num_connected = htons(numconnections);
-	send(sock, (char*)&num_connected, sizeof(num_connected), 0);
+	// printf("server buffer %x\n", buffer[1]);
+	int n = send(acceptsock, (char*)&ackbuffer, 2, 0);
+	if (n < 0) error("ERROR reading from socket");
+	else printf("success\n");
+	// while(1) {
+	// 	printf("sending\n");
+	// 	if(send(acceptsock, (char*)&buffer, 255, 0) < 0) {
+	// 		printf ("failed");
+	// 		return 1;
+	// 	}
 
-	/***** send username info : length string */
+	// }
+
+	// unsigned int num_connected = htons(numconnections);
+	// send(sock, (char*)&num_connected, sizeof(num_connected), 0);
+
+	/***** send username info : length string (use linked list) */
 
 	/***** check if username sent by client already exists */
-	
-
+	close(sock);
+	close(acceptsock);
 	return 0;
 }
+
+        // if( send(sock , message , strlen(message) , 0) < 0)
+        // {
+        //     puts("Send failed");
+        //     return 1;
+        // }
