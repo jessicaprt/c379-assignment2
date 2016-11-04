a6cdb21385941e6894fa8051d65518c6d92d56bc
Main User Account <jessicaprieto31@gmail.com>
2016-11-04 06:02:48 -0600

    handshake working from client

--- client/client.c
+++ client/client.c
@@ -10,7 +10,51 @@
 #include <poll.h>
  
 int sock;
- 
+
+int open_connection(char * hostname, char * port){
+    /*
+     * Following function grabbed from the getaddrinfo man page.
+     */
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    int sfd, s, j;
+
+    /* Obtain address(es) matching host/port */
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;          /* Any protocol */
+
+    s = getaddrinfo(hostname, port, &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "Unable to resolve hostname.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(rp->ai_family, rp->ai_socktype,
+                rp->ai_protocol);
+        if (sfd == -1)
+            continue;
+
+	if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+		break;                  /* Success */
+
+	close(sfd);
+    }
+
+    if (rp == NULL) {               /* No address succeeded */
+	    fprintf(stderr, "Could not connect\n");
+	    exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(result);           /* No longer needed */
+
+    return sfd;
+}
+
 void endconnection(int signum) {
     printf("c: closing socket\n");
     close(sock);
@@ -24,73 +68,65 @@ int main (int argc, char * argv[]) { //input    : chat379 hostname portnumber us
     char buffer[255];
     ssize_t read_size;
     int check;
-    struct pollfd sock_fds[200];
+    // struct pollfd sock_fds[200];
  
     signal(SIGTSTP, endconnection);
  
     if (argc != 4) {
         printf("Error, wrong number of arguments");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
  
     char *username = argv[3];
-    unsigned int username_length = strlen(username);
+    size_t username_length = strlen(username);
  
     if (username_length > 255) {
         fprintf(stderr, "Username is too long. Maximum username length is 255 characters\n");
         exit(EXIT_FAILURE);
     }
- 
-    int portnumber = atoi(argv[2]);
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    int sock = open_connection(argv[1], argv[2]);
  
     if (sock == -1) {
         printf("Error opening\n");
         exit(1);
     }
  
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
- 
-    bzero((char *) &srv_addr, sizeof(srv_addr));
-    bcopy((char *)server->h_addr, (char *)&srv_addr.sin_addr.s_addr, server->h_length);
-    srv_addr.sin_family = AF_INET;
-    srv_addr.sin_port = htons(portnumber);
- 
-    //establish connection to server
-    if(connect(sock,(struct sockaddr *) &srv_addr, sizeof(srv_addr)) == -1) {
-        perror("Error connnecting\n");
-    }
- 
     //Acknowledgement process
-    char ackbuffer[2];
-    unsigned char byte0, byte1;
+    const size_t buff_size = 1024;
+    buffer[buff_size];
+    // unsigned char byte0, byte1;
  
-    while (read_size = recv(sock, (char*)&ackbuffer, 2, 0) > 0){
-        //loops while waiting for bytes to be received
-        byte0 = *((unsigned char *)&ackbuffer);
-        byte1 = *((unsigned char *)&ackbuffer + 1);
-        break;
-    }
-   
-    printf("done receiving ack\n");
-    if (byte0 != 0xcf || byte1 != 0xa7) printf("byte failed\n");
- 
+    // while (read_size = recv(sock, buffer, 2, 0) > 0){
+    //     //loops while waiting for bytes to be received
+    //     byte0 = *((unsigned char *)&buffer);
+    //     byte1 = *((unsigned char *)&buffer + 1);
+    //     break;
+    // }
+
+    // if (byte0 != (char) 0xcf || byte1 != (char) 0xa7) printf("byte failed\n");
+    int s = 0;
+    s = recv(sock, buffer, buff_size, 0);
+	if (buffer[0] == (char) 0xCF && buffer[1] == (char) 0xA7){
+		fprintf(stdout, "Connected to server.\n");
+		send(sock, &username_length, sizeof(uint8_t), 0);
+		send(sock, username, username_length, 0);
+	} else {
+		fprintf(stdout, "%hhx, %hhx\n", buffer[0], buffer[1]);
+		fprintf(stderr, "Server does not speak protocol\n");
+	}
  
     /**** get number of connections from server; */
     char connections[2];
     unsigned char connected_str;
     unsigned int connected;
-    while (read_size = recv(sock, (char*)&connections, 2, 0) > 0){
-        //loops while waiting for bytes to be received
-        connected = atoi((char *)&connections); //should contain the number of users connected
-        printf("number of users: %i\n", connected);
-        printf("Users connected: ");
-        break;
-    }
+    printf("getting number of connections..\n");
+
+    s = recv(sock, (char*)&connections, 2, 0);
+    connected = atoi((char *)&connections); //should contain the number of users connected
+    printf("number of users: %i\n", connected);
+    printf("Users connected: ");
+
     char userinfolist[255];
     int i = 0;
     while(i<connected) {
